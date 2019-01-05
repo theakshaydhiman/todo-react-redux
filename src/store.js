e6037811265c11d080fcbb4d3bb30d477cd04ea5
Akshay Dhiman <akshay.virgo@gmail.com>
2019-01-05 16:24:30 +0530

    removed dependency redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,12 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleware = [thunk];
-
-const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(rootReducer, initialState, composeWithDevTools());
 
 export default store;
